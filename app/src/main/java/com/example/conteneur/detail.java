d5d96667f1fbbd09809fc34a7858df702e122a58
ALOUGBINE Dine Ola <doalougbine5@gmail.com>
2022-02-24 22:19:50 +0100

    troisième commit correction d'erreur

--- app/src/main/java/com/example/conteneur/detail.java
+++ app/src/main/java/com/example/conteneur/detail.java
@@ -10,15 +10,15 @@ public class detail extends AppCompatActivity {
     ImageView imagec;
     TextView compc,navc,numc,datec,detailsc;
     int[] img={R.drawable.maersk2,R.drawable.msc1,R.drawable.ever,R.drawable.cma1,R.drawable.titan};
-    String[] navire={"Maersk Seak","MSC","Hanjin","CMA CGM","Titan"};
+    String[] navire={"Maersk Seak","MSC Aliya","Hanjin","CMA CGM Jacques Saade","Ever Ace"};
     String[] numero={"MSKU1344","MSC0217","EMCU16783","CMAU1488","TTU1248"};
     String[] datedarriver={"12/12/2019","12/11/2019","02/10/2019","12/12/2020","12/12/2021"};
     String[] details={
-            "Le conteneur MSKU1344 a demarré du port le 12/12/2019 à 04h sur le navire Maersk Seak en transportant des marchandises pour stopper trois jours après au port de Yaounde",
-            "Le conteneur MSC0217 ademarré du port le 12/11/2019 à 06h45 sur  le navire MSC en transportant des marchandises automobiles pour sttopper deux jours après au port du burkina faso",
-            "Le conteneur EMCU16783 a demarré du port le 02/10/2020 à 08h sur le navire Hanjin en transportant des marchandise vestimentaires pour stopper une semaine apres au port d'Abuja",
-            "Le conteneur CMAU1488 a demarré du port le 12/12/2021 a 22h05 sur le navire CMA CGM en transportant des marchandises agroalimmentaires pour stopper un mois après au port du burkina",
-            "Le conteneur TTU1248 a demarré du port le 12/12/2021 a 06h45 sur le navire TITAN en transportant des marchandises immobilieres pour stopper au port de Yamoussokro"
+            "Le conteneur MSKU1344 a demarré du port de Barcelone le 01/12/2019 à 04h sur le navire Maersk Seak en transportant des marchandises en destination du port de Dakart",
+            "Le conteneur MSC0217 a demarré du port de Cotonou le 24/10/2019 à 06h45 sur  le navire MSC Aliya en transportant des marchandises automobiles en destination  du port de Mombasa",
+            "Le conteneur EMCU16783 a demarré du port d'Owendo le 17/09/2020 à 08h sur le navire Hanjin en transportant des produits vestimentaires en destination du port d'Alger",
+            "Le conteneur CMAU1488 a demarré du port de Cotonou le 10/12/2021 a 22h05 sur le navire CMA CGM Jacques Saade en transportant des produits agroalimmentaires en destination du port de Tema",
+            "Le conteneur TTU1248 a demarré du port de Tanger le 20/11/2021 a 06h45 sur le navire Ever Ace en transportant des produits vivrier en destination port de Lome"
         };
 
     @Override
@@ -30,7 +30,7 @@ public class detail extends AppCompatActivity {
         navc=findViewById(R.id.navire);
         numc=findViewById(R.id.numero);
         datec=findViewById(R.id.date);
-        detailsc=findViewById(R.id.date);
+        detailsc=findViewById(R.id.details);
 
         Intent intent=getIntent();
         String compagnie= intent.getStringExtra("compagnie");
@@ -41,7 +41,7 @@ public class detail extends AppCompatActivity {
         navc.setText(navire[pos]);
         numc.setText(numero[pos]);
         datec.setText(datedarriver[pos]);
-        details.setText(details[pos]);
+        detailsc.setText(details[pos]);
 
 
     }
